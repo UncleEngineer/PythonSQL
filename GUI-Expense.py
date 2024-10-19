4e2cd58ede52f66a77f618fcf2e28adad0909367
Uncle Engineer <loong.wissawakorn@gmail.com>
2024-10-19 15:04:19 +0700

    EP.2.3 checkpoint

--- GUI-Expense.py
+++ GUI-Expense.py
@@ -1,6 +1,7 @@
 from tkinter import *
 from tkinter import ttk
 from tkinter import messagebox
+import os
 
 
 ##########SQL##########
@@ -31,31 +32,57 @@ GUI.geometry('700x600')
 
 FONT1 = ('Angsana New',20)
 
+######TAB#######
+PATH = os.getcwd() # check current folder
+Tab = ttk.Notebook(GUI)
+Tab.pack(fill=BOTH,expand=1)
+
+T1 = Frame(Tab)
+T2 = Frame(Tab)
+
+icon_t1 = os.path.join(PATH,'form.png')
+icon_t2 = os.path.join(PATH,'history.png')
+iconimage_t1 = PhotoImage(file=icon_t1)
+iconimage_t2 = PhotoImage(file=icon_t2)
+
+
+Tab.add(T1,text='บันทึกค่าใช้จ่าย',image=iconimage_t1,compound='left')
+Tab.add(T2,text='ประวัติค่าใช้จ่าย',image=iconimage_t2,compound='left')
+
+
 #############
-L = Label(GUI,text='โปรแกรมบันทึกค่าใช้จ่าย',font=('Angsana New',30,'bold'))
+
+
+icon = os.path.join(PATH,'wallet.png')
+iconimage = PhotoImage(file=icon)
+
+L = Label(T1,image=iconimage)
+L.pack()
+
+L = Label(T1,text='โปรแกรมบันทึกค่าใช้จ่าย',font=('Angsana New',30,'bold'))
 L.pack(pady=10)
 #############
-L = Label(GUI,text='รายการ',font=FONT1)
+L = Label(T1,text='รายการ',font=FONT1)
 L.pack()
 
 v_title = StringVar()
-E1 = ttk.Entry(GUI,textvariable=v_title ,font=FONT1,width=50)
+E1 = ttk.Entry(T1,textvariable=v_title ,font=FONT1,width=50)
 E1.pack()
 
 #############
-L = Label(GUI,text='ราคา',font=FONT1)
+L = Label(T1,text='ราคา',font=FONT1)
 L.pack()
 
 v_price = StringVar()
-E2 = ttk.Entry(GUI,textvariable=v_price ,font=FONT1,width=50)
+E2 = ttk.Entry(T1,textvariable=v_price ,font=FONT1,width=50)
 E2.pack()
 
 #############
-L = Label(GUI,text='หมายเหตุ',font=FONT1)
+L = Label(T1,text='หมายเหตุ',font=FONT1)
 L.pack()
 
 v_others = StringVar()
-E3 = ttk.Entry(GUI,textvariable=v_others ,font=FONT1,width=50)
+E3 = ttk.Entry(T1,textvariable=v_others ,font=FONT1,width=50)
 E3.pack()
 
 
@@ -80,8 +107,21 @@ def Save(event=None):
 
 E3.bind('<Return>',Save) # ใส่ event=None ในฟังชั่น
 
-B1 = ttk.Button(GUI,text='Save',command=Save)
+B1 = ttk.Button(T1,text='Save',command=Save)
 B1.pack(ipadx=20,ipady=10,pady=20)
+
+########################TAB2#########################
+
+header = ['ID','รายการ','ค่าใช้จ่าย','หมายเหตุ','วัน-เวลา']
+hwidth = [50,200,100,200,100]
+
+table = ttk.Treeview(T2, columns=header,show='headings',height=20)
+table.pack()
+
+for h,w in zip(header,hwidth):
+    table.heading(h,text=h)
+    table.column(h,width=w)
+
 
 
 GUI.mainloop()
---
+++ basicpath.py
@@ -0,0 +1,9 @@
+import os
+
+PATH = os.getcwd()
+
+print(PATH)
+
+image_file = os.path.join(PATH,'wallet.png')
+
+print(image_file)
